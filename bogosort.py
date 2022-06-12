aeb84a015fb7779513aec329443f9ab7e57ecf34
Undercoverdog <64562491+Undercoverdog@users.noreply.github.com>
2022-06-12 16:39:52 +0200

    Add files via upload

--- bogosort.py
+++ bogosort.py
@@ -4,12 +4,16 @@ import random
 
 # generates and return a random array
 def randomArray():
-    length = random.randint(1, 105)
+    low = int(input("Lowest possible length "))
+    high = int(input("Highest possible length "))
+
+    length = random.randint(low, high + 1)
     arr = np.empty(length, dtype=int)
     for i in range(0, length):
         r = random.randint(-100, 100)
         arr[i] = r
     return arr
+
 
 # shuffles and returns an array
 def shuffleArray(pArray):
@@ -18,6 +22,7 @@ def shuffleArray(pArray):
         x = random.randint(i, y)
         pArray[i], pArray[x] = pArray[x], pArray[i]
     return pArray
+
 
 # checks if the array is sorted ascending and returns True or False
 def checkArray(pArray):
@@ -28,6 +33,7 @@ def checkArray(pArray):
 
     return True
 
+
 # bogo sort
 def bogoSort(pArray):
     print("Start" + str(pArray))
@@ -35,5 +41,23 @@ def bogoSort(pArray):
         shuffleArray(pArray)
     return pArray
 
+def fillArray(pNumbers):
+    tempList = pNumbers.split (",")
+    tempArr = np.empty(len(tempList), dtype=int)
+    for i in range(0, len(tempList)):
+        tempArr[i] = tempList[i]
+    return tempArr
 
-print("End result" + str(bogoSort(randomArray())))
+
+
+mode = input("[r]andom or [c]ustom array? ")
+if mode == "r" or mode == "random":
+    arr = randomArray();
+elif mode == "c" or mode  == "custom":
+    numbers = str(input("Enter comma separated integers. e.g. 1,2,3,4,5: "))
+    arr = fillArray(numbers)
+
+result = str(bogoSort(arr))
+
+
+print("End result: " + result)
